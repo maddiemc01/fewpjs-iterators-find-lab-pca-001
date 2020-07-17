ab403ca42096ad28dc41ef828c9a31ff51fdc32b
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-17 09:34:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teamArray) {
+  if (teamArray.find(team => team.result === "W")) {
+    let win = (teamArray.find(team => team.result === "W"))
+    return win.year
+  } else {
+    return undefined
+  }
+}
